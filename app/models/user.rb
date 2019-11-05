67a7179e9837398f09c1825b78720e3ee0a04dc2
BSarahB <bslajta@gmail.com>
2019-11-05 15:44:55 +0100

    adding file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+	after_create :welcome_send
+
+	
 	#Relation N-N entre les tables events et users doc necessite de passer par table jointe attendances
 	has_many :attendances
 	has_many :events, through: :attendances 
@@ -7,4 +10,8 @@ class User < ApplicationRecord
 	#3)dans la classe User on dit que user a plusieurs event_administrators(admins), ces administrators correspondent a la colonne admin_id dans le model Event
 	has_many :event_admins, foreign_key: 'admin_id', class_name: "Event" 
 
+
+  	def welcome_send
+   	 UserMailer.welcome_email(self).deliver_now
+  	end
 end
