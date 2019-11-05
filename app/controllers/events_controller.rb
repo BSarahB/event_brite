272044fabfe083bccfeb9358e083eef318b59924
BSarahB <bslajta@gmail.com>
2019-11-06 00:17:51 +0100

    link for event id

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,14 +1,30 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :show]
 
-  def index
+    def index
   	    # Méthode qui récupère tous les evenements et les envoie à la view index (index.html.erb) pour affichage
-  	@events = Event.all
-  end
+   	  @events = Event.all
+    end
  
- def new
+   def new
  	    # Méthode qui crée un Evenement vide et l'envoie à une view qui affiche le formulaire pour 'le remplir' (new.html.erb)
     @event = Event.new
-  end
+    end
 
+    def show
+    	    # Méthode qui récupère l evenement  concerné et l'envoie à la view show (show.html.erb) pour affichage
+    @event = Event.find(params[:id])
+  	end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
 end
