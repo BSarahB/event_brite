27873e0c10eeb02851d7302f806cc6e038db65ca
BSarahB <bslajta@gmail.com>
2019-11-06 02:36:06 +0100

    fonctionnel create event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -17,7 +17,33 @@ class EventsController < ApplicationController
   	end
 
     def create
-    end
+    # Méthode qui créé un evemenent à partir du contenu du formulaire de new.html.erb, soumis par l'utilisateur
+    # pour info, le contenu de ce formulaire sera accessible dans le hash params (ton meilleur pote)
+    # Une fois la création faite, on redirige généralement vers la méthode show (pour afficher le potin créé)
+
+    #on verifie que le formulaire envoie bien les params saisis code entre les puts $ non necessaire , mais qui nous permet de checker au debut 
+    puts "$" * 60
+    puts "ceci est le contenu de params et signifie que le formulaire pointe bien sur create :"
+    puts params
+    puts "$" * 60
+
+    #le code suivant est le code a rentrer dans $ rails c afin de creer en BDD un user test un event test qui a donc ce user associe. et on check si il y les rollbacks et si ca marche bien
+    #e = Event.new(blabla)
+    #e.save
+    #sil n y a pas de rollbacks notre save est bien effectue et notre @gossip sauve en BDD
+    @event = Event.new(start_date: params[:start_date], duration: params[:duration], title: params[:title], description: params[:description], price: params[:price], location: params[:location], admin_id: 1)
+ #=> {:title=>["must exist"], :title=>["is too short (minimum is 3 characters)"], :content=>["can't be blank"] etc exemple de rollbacks}
+
+    if @event.save # essaie de sauvegarder en base @event
+      flash[:success] = "The super Event was succesfully saved !"
+    # si ça marche, il redirige vers la page d'index du site
+        redirect_to events_path #redirect_to va passer   /   eventss#index cf $ rails routes | grep root
+
+  else
+    # sinon, il render la view new (qui est celle sur laquelle on est déjà)
+    render :new
+  end
+end
 
     def edit
     end
